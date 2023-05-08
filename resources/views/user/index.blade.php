8ff1697490ff63693e68864d8ef6f446fc61488d
cakmalik <hasinilmalik@gmail.com>
2023-05-09 04:07:57 +0700

    bismillah

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,15 @@
+@seoTitle(__('Dashboard'))
+
+<x-app-layout>
+    <x-slot:header>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+        </x-slot>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <x-splade-table :for="$users" />
+            </div>
+        </div>
+</x-app-layout>
