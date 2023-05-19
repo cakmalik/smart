11cb29c9aabd649b9d0606ca9797184892fa54e6
cakmalik <hasinilmalik@gmail.com>
2023-05-20 04:53:47 +0700

    feat(admission invoice): add AdmissionInvoiceController, StoreAdmissionInvoiceRequest, UpdateAdmissionInvoiceRequest, AdmissionInvoicePolicy, AdmissionInvoiceFactory, and create_admission_invoices_table migration and AdmissionInvoiceSeeder
    This commit adds the following files:
    - AdmissionInvoiceController.php: a controller for handling admission invoice related requests
    - StoreAdmissionInvoiceRequest.php: a form request for validating admission invoice store requests
    - UpdateAdmissionInvoiceRequest.php: a form request for validating admission invoice update requests
    - AdmissionInvoicePolicy.php: a policy for authorization of admission invoice related actions
    - AdmissionInvoiceFactory.php: a factory for generating admission invoice model instances
    - create_admission_invoices_table.php: a migration for creating the admission_invoices table in the database
    - AdmissionInvoiceSeeder.php: a seeder for populating the admission_invoices table with sample data
    
    These files were added to support the implementation of admission invoice functionality in the application.

---
+++ app/Http/Controllers/AdmissionInvoiceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAdmissionInvoiceRequest;
+use App\Http\Requests\UpdateAdmissionInvoiceRequest;
+use App\Models\AdmissionInvoice;
+
+class AdmissionInvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAdmissionInvoiceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AdmissionInvoice $admissionInvoice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AdmissionInvoice $admissionInvoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAdmissionInvoiceRequest $request, AdmissionInvoice $admissionInvoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AdmissionInvoice $admissionInvoice)
+    {
+        //
+    }
+}
