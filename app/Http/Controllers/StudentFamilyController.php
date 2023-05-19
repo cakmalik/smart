e28cfaf282167ebeadcda350499c9253fd7765af
cakmalik <hasinilmalik@gmail.com>
2023-05-20 04:25:00 +0700

    feat(student-family): add StudentFamily resource with controller, requests, policy, model, migration, and seeder
    Add a new resource for StudentFamily with its corresponding controller, requests, policy, model, migration, and seeder. This will allow us to manage student family data in our application.

---
+++ app/Http/Controllers/StudentFamilyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreStudentFamilyRequest;
+use App\Http\Requests\UpdateStudentFamilyRequest;
+use App\Models\StudentFamily;
+
+class StudentFamilyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreStudentFamilyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StudentFamily $studentFamily)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(StudentFamily $studentFamily)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateStudentFamilyRequest $request, StudentFamily $studentFamily)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StudentFamily $studentFamily)
+    {
+        //
+    }
+}
