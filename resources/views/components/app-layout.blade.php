895b1db65935cc5274b255a6c8e2cd32f4bb1bf5
cakmalik <hasinilmalik@gmail.com>
2023-05-19 16:00:22 +0700

    style(app-layout.blade.php): fix indentation and add missing closing div tag style(banner.blade.php): improve readability by formatting code and adding line breaks style(dashboard.blade.php): fix indentation and add missing closing div tag The changes in these files are purely cosmetic and aim to improve the readability and maintainability of the code. Indentation has been fixed, missing closing div tags have been added, and code has been formatted to improve readability.

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -6,14 +6,14 @@
     <!-- Page Heading -->
     @isset($header)
         <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <div class="max-w-7xl mx-auto py-3 px-4 sm:px-6 lg:px-8">
                 {{ $header }}
             </div>
         </header>
-    @endif
+        @endif
 
-    <!-- Page Content -->
-    <main>
-        {{ $slot }}
-    </main>
-</div>
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
