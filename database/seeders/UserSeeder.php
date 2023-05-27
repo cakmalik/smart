4e126d8178b775fe7488b1838fad54bd4e90a21e
cakmalik <hasinilmalik@gmail.com>
2023-05-28 02:59:47 +0700

    feat(permission): add spatie/laravel-permission package to manage roles and permissions fix(middleware): fix middleware declaration in Kernel.php to avoid syntax error del(migration): delete unused migration file for sessions table creation
    feat(migrations): add permission tables migration to support spatie/laravel-permission package
    feat(seeder): add admin and santri roles and permissions to UserSeeder
    feat(web.php): add admin and santri middleware to user resource routes to restrict access based on roles

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,9 +4,11 @@ namespace Database\Seeders;
 
 use App\Models\Team;
 use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 use App\Repositories\UserRepositoryInterface;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
@@ -15,15 +17,34 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $role = Role::create(['name' => 'admin']);
+        $santriRole = Role::create(['name' => 'santri']);
+        $permission = Permission::create(['name' => 'manage users']);
+        $permissionSantri = Permission::create(['name' => 'manage family']);
+        $role->givePermissionTo($permission);
+        $santriRole->givePermissionTo($permissionSantri);
+
         User::create([
-            'name' => 'John Doe',
-            'email' => 'm@m.m',
+            'name' => 'Admin',
+            'email' => 'admin@bakid.com',
             'password' => bcrypt('password'),
-        ]);
+        ])->assignRole('admin');
+
+        User::create([
+            'name' => 'Santri bakid',
+            'email' => 'santri@bakid.com',
+            'password' => bcrypt('password'),
+        ])->assignRole('santri');
 
         Team::create([
             'user_id' => 1,
             'name' => 'John Doe',
+            'personal_team' => true,
+        ]);
+
+        Team::create([
+            'user_id' => 2,
+            'name' => 'Santri bakid',
             'personal_team' => true,
         ]);
 
