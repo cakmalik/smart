083375f62fd53c60ebbbf3f0dd3cc4db15b6616b
cakmalik <hasinilmalik@gmail.com>
2023-06-01 05:38:54 +0700

    feat(student): add name field to student table and make it fillable fix(student): allow StoreStudentRequest to authorize requests fix(student): make name field required in StoreStudentRequest refactor(student): make religion and address fields nullable in student table feat(student): add support for file upload in student creation form feat(student): add student_image and parent_image fields to student creation form fix(student): add missing $guarded property to Student model chore(composer): add laravel-dumper and laravel-debugbar to dev dependencies chore(student): add debugbar and storage/debugbar to .gitignore

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Student extends Model
 {
     use HasFactory;
+    protected $guarded = [];
     public function user()
     {
         return $this->belongsTo(User::class);
