8c6795731ae1580b96f2cc08741ff4858b6b7104
cakmalik <hasinilmalik@gmail.com>
2023-06-01 09:44:43 +0700

    add pattern

---
+++ app/Services/User/UserService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services\User;
+
+use LaravelEasyRepository\BaseService;
+
+interface UserService extends BaseService
+{
+    /**
+     * getByEmail
+     *
+     * @param  mixed $email
+     * @return void
+     */
+    public function getByEmail($email);
+}
