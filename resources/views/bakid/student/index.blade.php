b143ddc68aa694b0d238e413ebc4b7f368b7f353
cakmalik <hasinilmalik@gmail.com>
2023-06-10 06:41:31 +0700

    ok

---
+++ resources/views/bakid/student/index.blade.php
@@ -0,0 +1,55 @@
+@seoTitle(__('Users'))
+
+<x-app-layout>
+    <x-slot:header>
+        <h2 class="font-medium text-xl text-gray-800">
+            {{ __('bakid.t.family_members') }}
+        </h2>
+        </x-slot>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                {{-- <Link slideover href="{{ route('user.create') }}">
+                <button
+                    class="bg-slate-500 p-2 text-white rounded-md text-sm items-center  hover:bg-green-500 w-20 mb-4">
+                    Tambah
+                </button>
+                </Link>
+                <button
+                    class="bg-slate-500 p-2 text-white rounded-md text-sm items-center ms-2 hover:bg-green-500 w-20 mb-4">
+                    Coba
+                </button>
+                </Link> --}}
+                <x-splade-table :for="$students" striped>
+                    <x-slot name="empty-state">
+                        <p class="text-center">No students found.</p>
+                    </x-slot>
+                    {{-- @cell('image', $students)
+                        <img src="{{ $user->image }}" alt="{{ $user->name }}" class="rounded-full h-10 w-10">
+                    @endcell --}}
+                    @cell('action', $students)
+                        <div class="flex gap-1 ">
+                            <Link href="{{ route('user.show', $students->id) }}"
+                                class="rounded-full p-1 bg-slate-500 text-white hover:bg-green-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-5 h-5 p-1">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM10.5 7.5v6m3-3h-6" />
+                            </svg>
+
+                            </Link>
+                            <Link modal href="{{ route('user.edit', $students->id) }}"
+                                class="rounded-full p-1 bg-slate-500 text-white hover:bg-green-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-5 h-5 p-1">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                            </svg>
+                            </Link>
+                        </div>
+                    @endcell
+                </x-splade-table>
+
+            </div>
+        </div>
+</x-app-layout>
