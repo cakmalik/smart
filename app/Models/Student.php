1c6165745922ef334f5120526b3adee265731f67
cakmalik <hasinilmalik@gmail.com>
2023-06-10 06:40:54 +0700

    feat(StudentController.php): add LocationService dependency injection to StudentController constructor feat(StudentController.php): add global search and pagination to index method feat(StudentController.php): add toast notification after successful data storage refactor(StudentController.php): change StoreStudentRequest validation rules to only required fields refactor(Student.php): add attribute casting for gender, dateOfBirth, name, province, city, district, and village feat(AppServiceProvider.php): add default date, time, and datetime format for Input component feat(LocationService.php): create LocationService interface for location-related services
    The changes in this commit include adding support for global search and pagination in the index method of StudentController, adding toast notification after successful data storage, and changing StoreStudentRequest validation rules to only required fields. Additionally, attribute casting for gender, dateOfBirth, name, province, city, district, and village has been added to Student.php. The default date, time, and datetime format for Input component

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -3,15 +3,84 @@
 namespace App\Models;
 
 use App\Models\User;
+use App\Services\Location\LocationServiceImplement;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Student extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    // private $loc;
+
+    // public function __construct(LocationService $locationService)
+    // {
+    //     parent::__construct();
+    //     $loc = $locationService;
+    // }
+
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    protected function gender(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => 'female' ? 'Perempuan' : 'Laki-laki',
+            set: fn (string $value) => 'Perempuan' ? 'female' : 'male'
+        );
+    }
+    protected function dateOfBirth(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) =>  date('Y-m-d', strtotime($value))
+        );
+    }
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn (String $value) => ucwords($value),
+            set: fn (String $value) => strtolower($value)
+        );
+    }
+
+    protected function province(): Attribute
+    {
+        $loc = new LocationServiceImplement();
+        return Attribute::make(
+            get: fn (string $value) => ucwords($value),
+            set: fn (string $value) => $loc->getProvinceName($value)
+        );
+    }
+
+    protected function city(): Attribute
+    {
+        $loc = new LocationServiceImplement();
+        return Attribute::make(
+            get: fn (string $value) => ucwords($value),
+            set: fn (string $value) => $loc->getCityName($value)
+        );
+    }
+
+    protected function district(): Attribute
+    {
+        $loc = new LocationServiceImplement();
+        return Attribute::make(
+            get: fn (string $value) => ucwords($value),
+            set: fn (string $value) => $loc->getDistrictName($value)
+        );
+    }
+
+    protected function village(): Attribute
+    {
+        $loc = new LocationServiceImplement();
+        return Attribute::make(
+            get: fn (string $value) => ucwords($value),
+            set: fn (string $value) => $loc->getVillageName($value)
+        );
     }
 }
