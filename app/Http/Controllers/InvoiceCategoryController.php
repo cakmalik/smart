7b732a4aec7a28b9f750995c098e89cf8d9bc168
cakmalik <hasinilmalik@gmail.com>
2023-06-10 14:09:41 +0700

    feat(controller): add InvoiceCategoryController and InvoiceController with CRUD methods feat(requests): add StoreInvoiceCategoryRequest, UpdateInvoiceCategoryRequest, StoreInvoiceRequest, and UpdateInvoiceRequest fix(controller): add StudentService dependency injection to StudentController constructor refactor(controller): replace Student model creation with StudentService method call in StudentController@store method fix(requests): add numeric validation rules for child_number and siblings in StoreStudentRequest

---
+++ app/Http/Controllers/InvoiceCategoryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreInvoiceCategoryRequest;
+use App\Http\Requests\UpdateInvoiceCategoryRequest;
+use App\Models\InvoiceCategory;
+
+class InvoiceCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInvoiceCategoryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(InvoiceCategory $invoiceCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(InvoiceCategory $invoiceCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInvoiceCategoryRequest $request, InvoiceCategory $invoiceCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(InvoiceCategory $invoiceCategory)
+    {
+        //
+    }
+}
