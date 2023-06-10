4760037026648543ca7932581bf8e3d7a972ed55
cakmalik <hasinilmalik@gmail.com>
2023-06-11 01:49:59 +0700

    install package

---
+++ resources/lang/id/message.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'welcome' => 'Selamat datang di halaman admin',
+    'dashboard' => 'Dasbor',
+
+    'manage_account' => 'Kelola Akun',
+    'sign_out' => 'Keluar',
+    'dashboard' => 'Beranda',
+    'family_member' => 'Keluarga',
+];
