590fa2b58621aa80d98af0a3c2527eec4f31ae1a
cakmalik <hasinilmalik@gmail.com>
2023-06-11 13:45:43 +0700

    refactor(helpers.php): refactor getRandomAyat function to return a more comprehensive data structure containing arabic text, translation, surah name, surah number, and verse number fix(app.js): disable progress bar in SpladePlugin to improve user experience refactor(welcome.blade.php): change displayed text to show arabic text and add surah name, surah number, and verse number if available

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,6 +14,6 @@ createApp({
     .use(SpladePlugin, {
         max_keep_alive: 10,
         transform_anchors: false,
-        progress_bar: true,
+        progress_bar: false,
     })
     .mount(el);
