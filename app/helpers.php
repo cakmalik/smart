ef4a4ba98903a8cfa8838ea8bd588b50d8782fbc
cakmalik <hasinilmalik@gmail.com>
2023-06-11 13:53:28 +0700

    fix(helpers.php): handle exception when calling external API and log the error message feat(helpers.php): return null when exception occurs instead of returning null array feat(welcome.blade.php): add null check to prevent error when $rand is null

--- app/helpers.php
+++ app/helpers.php
@@ -1,39 +1,45 @@
 <?php
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 if (!function_exists('getRandomAyat')) {
     function getRandomAyat()
     {
-        $response = Http::get('https://quran-api-id.vercel.app/random');
+        try {
+            $response = Http::get('https://quran-api-id.vercel.app/random');
 
-        if ($response->successful()) {
-            $data = $response->json();
-            $fix['ayat'] = $data['arab'];
-            $fix['arti'] = $data['translation'];
+            if ($response->successful()) {
+                $data = $response->json();
+                $fix['ayat'] = $data['arab'];
+                $fix['arti'] = $data['translation'];
 
-            $secondaryUrl  = $data['image']['secondary'];
+                $secondaryUrl  = $data['image']['secondary'];
 
-            $pattern_surat_ke = "/\/(\d+)\//";
-            preg_match($pattern_surat_ke, $secondaryUrl, $matches);
-            $surat_ke = $matches[1];
+                $pattern_surat_ke = "/\/(\d+)\//";
+                preg_match($pattern_surat_ke, $secondaryUrl, $matches);
+                $surat_ke = $matches[1];
 
-            $pattern_ayat_ke = "/\/\d+\/(\d+)/";
-            preg_match($pattern_ayat_ke, $secondaryUrl, $matches);
-            $ayat_ke = $matches[1];
+                $pattern_ayat_ke = "/\/\d+\/(\d+)/";
+                preg_match($pattern_ayat_ke, $secondaryUrl, $matches);
+                $ayat_ke = $matches[1];
 
 
-            $surah = Http::get('https://quran-api-id.vercel.app/surahs/' . $surat_ke);
-            if ($surah->successful()) {
-                $surah = $surah->json();
-                $fix['surat'] = $surah['name'];
-                $fix['surat_ke'] = $surat_ke;
-                $fix['ayat_ke'] = $ayat_ke;
+                $surah = Http::get('https://quran-api-id.vercel.app/surahs/' . $surat_ke);
+                if ($surah->successful()) {
+                    $surah = $surah->json();
+                    $fix['surat'] = $surah['name'];
+                    $fix['surat_ke'] = $surat_ke;
+                    $fix['ayat_ke'] = $ayat_ke;
+                }
+
+                return $fix;
             }
 
-            return $fix;
+            return null;
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return null;
         }
-
-        return null;
     }
 }
