a6d725885190b4b92b2ea2778eb253381a5013b3
cakmalik <hasinilmalik@gmail.com>
2023-06-13 02:32:45 +0700

    feat(StudentController.php, User.php, edit.blade.php, show.blade.php, welcome.blade.php): add isAdmin method to User model and conditionally show content based on user role in views

--- resources/views/bakid/student/show.blade.php
+++ resources/views/bakid/student/show.blade.php
@@ -1,21 +1,18 @@
 <x-app-layout>
     <x-splade-modal>
         <x-splade-form :default="$student" :action="route('student.update', $student->id)" class="flex flex-col gap-4" method="put">
-            {{-- @dd($student) --}}
             <div class="grid grid-cols-3 gap-4">
                 <div class="">
                     <img class="rounded-xl object-cover w-full h-full"
                         src="{{ $student->student_image ? asset('storage/student-photos/' . $student->student_image) : asset('bakid/default_image.jpg') }}">
                 </div>
                 <div class="col-span-2">
-                    <h3 class="text-2xl">{{ $student->name }}</h3>
-                    <h3 class="text-gray-500 italic">{{ $student->nickname }}</h3>
+                    <h3 class="text-xl">{{ $student->name }}</h3>
+                    <h3 class="text-gray-500">{{ $student->nickname }}</h3>
                     <h3>{{ $student->district . ' - ' . $student->city }}</h3>
                     <h3>Asrama </h3>
                 </div>
-
             </div>
-
         </x-splade-form>
     </x-splade-modal>
 </x-app-layout>
