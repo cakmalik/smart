d293a1f961931eea0ba75ab63f744f95be85f303
cakmalik <hasinilmalik@gmail.com>
2023-06-12 08:09:23 +0700

    feat(Add/Background1.php, Card/Summary.php, public/*, resources/*): add new components and assets for the app
    Add two new components, Background1 and Summary, to the Add and Card namespaces respectively. These components are used to render the background and summary sections of the app.
    
    Add new assets to the public directory, including two new background images (bg1.jpg and bg2.jpg), a blob animation SVG (blobanimation.svg), and a new font file (usmani.otf).
    
    Update the app.css file to include the new font file.
    
    Update the app-layout.blade.php file to use the new background image and update the navigation bar.
    
    Update the summary.blade.php file to include a new div element for the summary section.
    
    feat(navigation.blade.php): add commented out admin-only navigation menu to be able to easily enable it in the future
    
    style(welcome.blade.php): remove unnecessary backdrop-blur-md class from divs
    style(dashboard.blade.php): add grid layout to summary cards and adjust gap sizes
    feat(tailwind.config.js): add keyframes and animation for blob effect on elements

---
+++ resources/views/components/card/summary.blade.php
@@ -0,0 +1,3 @@
+<div class="bg-white/50 hover:bg-white py-3 px-5 rounded-lg shadow-lg text-white text-center backdrop-blur-md">
+    sdf
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,23 @@ module.exports = {
             fontFamily: {
                 primary: "Usmani",
             },
+            animation: { blob: "blob 4s infinite" },
+            keyframes: {
+                blob: {
+                    "0%": {
+                        transform: "translate(0px, 0px) scale(1)",
+                    },
+                    "33%": {
+                        transform: "translate(40px, -60px) scale(1.2)",
+                    },
+                    "66%": {
+                        transform: "translate(-30px, 30px) scale(0.8)",
+                    },
+                    "100%": {
+                        transform: "translate(0px, 0px) scale(1)",
+                    },
+                },
+            },
         },
     },
 
