cf1fea14f8fc6536fe71f58e0c84085f6d0e002c
cakmalik <hasinilmalik@gmail.com>
2023-06-12 15:42:55 +0700

    fix(StudentController.php): comment out unused code to improve code readability feat(StudentController.php): add support for internationalization by adding language files for Splade package in multiple languages
    feat(lang): add translations for Indonesian, Italian, Japanese, Malay, Dutch, Portuguese (Brazil), Portuguese (Portugal), and Russian
    
    This commit adds translations for various languages to the application. The added languages are Indonesian, Italian, Japanese, Malay, Dutch, Portuguese (Brazil), Portuguese (Portugal), and Russian. These translations will help users who speak these languages to use the application more easily and effectively.
    
    feat(lang): add translations for Ukrainian, Urdu, and Simplified Chinese languages
    fix(student): fix student image not showing and add student details in show view

--- resources/views/bakid/student/show.blade.php
+++ resources/views/bakid/student/show.blade.php
@@ -1,18 +1,21 @@
 <x-app-layout>
     <x-splade-modal>
         <x-splade-form :default="$student" :action="route('student.update', $student->id)" class="flex flex-col gap-4" method="put">
-            @dd($student)
-            <div class="grid grid-cols-2">
+            {{-- @dd($student) --}}
+            <div class="grid grid-cols-3 gap-4">
                 <div class="">
-                    <img src="{{ asset('storage/student-photos/' . $student->student_image) }}" alt="">
+                    <img class="rounded-xl object-cover w-full h-full"
+                        src="{{ $student->student_image ? asset('storage/student-photos/' . $student->student_image) : asset('bakid/default_image.jpg') }}">
                 </div>
-                <div class="">dfs</div>
+                <div class="col-span-2">
+                    <h3 class="text-2xl">{{ $student->name }}</h3>
+                    <h3 class="text-gray-500 italic">{{ $student->nickname }}</h3>
+                    <h3>{{ $student->district . ' - ' . $student->city }}</h3>
+                    <h3>Asrama </h3>
+                </div>
 
             </div>
-            <button type="submit"
-                class="bg-slate-500 p-2 text-white rounded-md text-sm items-center  hover:bg-green-500 w-20">
-                Update
-            </button>
+
         </x-splade-form>
     </x-splade-modal>
 </x-app-layout>
