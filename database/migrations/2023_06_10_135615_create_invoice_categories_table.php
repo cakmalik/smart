be3dd13a714d523b1dee2e21a3a0113b653e127b
cakmalik <hasinilmalik@gmail.com>
2023-06-15 03:42:11 +0700

    feat(database): add status and verified_at columns to students table to support student verification process refactor(database): change amount column type in invoice_categories table from string to bigInteger to improve data consistency feat(database): add InvoiceCategorySeeder to seed initial invoice categories data

--- database/migrations/2023_06_10_135615_create_invoice_categories_table.php
+++ database/migrations/2023_06_10_135615_create_invoice_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('invoice_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('amount');
+            $table->bigInteger('amount');
             $table->timestamps();
         });
     }
