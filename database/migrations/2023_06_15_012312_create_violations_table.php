ee49beae6369579d75fb3a8669f2100303c78bf8
cakmalik <hasinilmalik@gmail.com>
2023-06-15 03:13:18 +0700

    feat(migrations): add dormitories, rooms, violations, student_violations, student_educational_backgrounds, in_out_permission_types, and student_in_out_permissions tables
    Add dormitories, rooms, and violations tables with their respective columns to store data related to dormitories, rooms, and violations. Add student_violations table to store data related to violations that are committed by students. Add student_educational_backgrounds table to store data related to the educational background of students. Add in_out_permission_types table to store data related to the types of in-out permissions that can be given to students. Add student_in_out_permissions table to store data related to the in-out permissions that are given to students.

--- database/migrations/2023_06_15_012312_create_violations_table.php
+++ database/migrations/2023_06_15_012312_create_violations_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('violations', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('point')->default(0);
+            $table->enum('type', ['ringan', 'sedang', 'berat']);
             $table->timestamps();
         });
     }
