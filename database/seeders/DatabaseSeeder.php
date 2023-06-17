2a379bbd18bf3fbc5368dba8e804cb1bc78961fa
cakmalik <hasinilmalik@gmail.com>
2023-06-17 14:47:41 +0700

    group dropdown ok

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\AdmissionSeeder;
+use Database\Seeders\PaymentMethodSeeder;
 use Database\Seeders\InvoiceCategorySeeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,7 +18,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             AdmissionSeeder::class,
-            InvoiceCategorySeeder::class
+            InvoiceCategorySeeder::class,
+            PaymentMethodSeeder::class,
         ]);
     }
 }
