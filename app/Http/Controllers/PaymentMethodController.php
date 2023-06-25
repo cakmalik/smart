d9cada37a38b5506cc324c3d6546630d46b99df6
cakmalik <hasinilmalik@gmail.com>
2023-06-25 12:49:20 +0700

    feat(invoice, payment): add payment method selection feature to invoice payment process
    - Add PaymentMethodController with chooseMethod and changeMethod methods to handle payment method selection and update invoice payment method
    - Update route for payment method selection and update
    - Add Payment Instructions section to invoice show page
    - Add invoice details to invoice show page

--- app/Http/Controllers/PaymentMethodController.php
+++ app/Http/Controllers/PaymentMethodController.php
@@ -2,65 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
+use Illuminate\Http\Request;
+use App\Models\PaymentMethod;
 use App\Http\Requests\StorePaymentMethodRequest;
 use App\Http\Requests\UpdatePaymentMethodRequest;
-use App\Models\PaymentMethod;
 
 class PaymentMethodController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function chooseMethod($invoice_number)
     {
-        //
+        $invoice = Invoice::where('invoice_number', $invoice_number)->first();
+        if ($invoice->payment_method_id != null) {
+            return redirect()->route('invoice.show', $invoice->invoice_number);
+        }
+        $payment_methods = PaymentMethod::all();
+        // dd($payment_methods);
+        return view('invoice.choose_method', compact('invoice', 'payment_methods'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function changeMethod(Request $request)
     {
-        //
-    }
+        try {
+            if ($request->payment_method_id == null) {
+                // TODO:ini nanti diganti code
+                $payment_method = PaymentMethod::where('name', '==', 'Cash')->first()->id;
+            } else {
+                $payment_method = $request->payment_method_id;
+            }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StorePaymentMethodRequest $request)
-    {
-        //
-    }
+            $invoice = Invoice::where('invoice_number', $request->invoice_number)->first();
+            $invoice->payment_method_id = $payment_method;
+            $invoice->save();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(PaymentMethod $paymentMethod)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(PaymentMethod $paymentMethod)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdatePaymentMethodRequest $request, PaymentMethod $paymentMethod)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(PaymentMethod $paymentMethod)
-    {
-        //
+            return redirect()->route('pay.invoice', $invoice->invoice_number);
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Terjadi kesalahan, silahkan coba lagi');
+        }
+        // return redirect()->route('pay.invoice', $invoice->invoice_number);
     }
 }
