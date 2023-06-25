826906455c2903d22b24d777949bd9b829c8adc1
cakmalik <hasinilmalik@gmail.com>
2023-06-25 13:58:59 +0700

    feat(InvoiceUtility): add InvoiceUtilityController, StoreInvoiceUtilityRequest, UpdateInvoiceUtilityRequest, InvoiceUtilityPolicy, InvoiceUtilityFactory, and InvoiceUtility migration and seeder files to support CRUD operations for InvoiceUtility model.

---
+++ app/Http/Controllers/InvoiceUtilityController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreInvoiceUtilityRequest;
+use App\Http\Requests\UpdateInvoiceUtilityRequest;
+use App\Models\InvoiceUtility;
+
+class InvoiceUtilityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInvoiceUtilityRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(InvoiceUtility $invoiceUtility)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(InvoiceUtility $invoiceUtility)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInvoiceUtilityRequest $request, InvoiceUtility $invoiceUtility)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(InvoiceUtility $invoiceUtility)
+    {
+        //
+    }
+}
