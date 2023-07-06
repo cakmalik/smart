6aa039020d2d58c62c6a61ac713ee39b2a227127
cakmalik <hasinilmalik@gmail.com>
2023-07-07 06:37:04 +0700

    add exit btn

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,15 +2,21 @@
 
 <x-app-layout>
     <x-slot:header>
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <div class="flex justify-between">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                {{ __('Dashboard') }}
+            </h2>
+            <Link confirm="Apakah yakin keluar?" confirm-button="Ya!" cancel-button="Tidak" href="{{ route('logout') }}"
+                method="post">
+            Keluar
+            </Link>
+        </div>
         </x-slot>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 ">
                 @if (auth()->user()->students->count() != 0)
-                    <div class="grid grid-cols-2 sm:grid-cols-4 gap-2 sm:gap-3 mb-4">
+                    <div class="grid grid-cols-2 gap-2 mb-4 sm:grid-cols-4 sm:gap-3">
                         {{-- <x-card.summary />
                         <x-card.summary />
                         <x-card.summary />
@@ -20,7 +26,7 @@
                         <x-dashboard.santri-baru :data="$x" />
                     </x-splade-rehydrate>
                 @else
-                    <div class="bg-white/30 overflow-hidden shadow-xl sm:rounded-lg p-5 backdrop-blur-md">
+                    <div class="p-5 overflow-hidden shadow-xl bg-white/30 sm:rounded-lg backdrop-blur-md">
                         <x-welcome />
                     </div>
                 @endif
