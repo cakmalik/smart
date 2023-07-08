706c32927e3f937e5b865dfc4d34adf0a6b757aa
cakmalik <hasinilmalik@gmail.com>
2023-07-09 02:44:17 +0700

    make migration format message

---
+++ app/Http/Controllers/FormatMessageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreFormatMessageRequest;
+use App\Http\Requests\UpdateFormatMessageRequest;
+use App\Models\FormatMessage;
+
+class FormatMessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFormatMessageRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FormatMessage $formatMessage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FormatMessage $formatMessage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFormatMessageRequest $request, FormatMessage $formatMessage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FormatMessage $formatMessage)
+    {
+        //
+    }
+}
