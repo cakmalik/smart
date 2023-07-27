e8d1e781331efdf0ed44aff6bad8ddc98439d35e
cakmalik <hasinilmalik@gmail.com>
2023-07-28 03:56:42 +0700

    add change message tagar

--- app/helpers.php
+++ app/helpers.php
@@ -69,3 +69,23 @@ if (!function_exists('invoiceNumber')) {
         return 'INV-' . $student_id . '-' . date('YmdHis');
     }
 }
+
+if (!function_exists('changeMessageTagar')) {
+    function changeMessageTagar($for)
+    {
+        public static function replaceVariables($variabel, $pesan, $nama, $posisi)
+        {
+            switch ($variabel) {
+                case 'nama':
+                    $pesan = str_replace("#nama", $nama, $pesan);
+                    break;
+                case 'posisi':
+                    $pesan = str_replace("#posisi", $posisi, $pesan);
+                    break;
+                // Tambahkan case lain sesuai kebutuhan
+            }
+
+            return $pesan;
+        }
+    }
+}
