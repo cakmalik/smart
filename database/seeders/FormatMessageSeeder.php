b2beb0d7f82d72501e4339044ea60f69f0816c9b
cakmalik <hasinilmalik@gmail.com>
2023-07-29 04:01:58 +0700

    change value seeder message

--- database/seeders/FormatMessageSeeder.php
+++ database/seeders/FormatMessageSeeder.php
@@ -17,7 +17,7 @@ class FormatMessageSeeder extends Seeder
             [
                 'name' => 'reminder_registration',
                 'label' => 'Pengingat Pendaftaran',
-                'message' => 'Assalamualaikum #nama_ortu, #enter #enter Terima kasih telah berkenan untuk mendaftar di PP Miftahul Ulum Bakid. #enter Kami mengingatkan bahwa hari ini Pendaftaran Santri Baru telah dibuka. #enter #enter Jangan lupa melanjutkan pengisian data santri di website bakid.id #enter #enter Terima kasih, #enter #panitia_psb_2023',
+                'message' => 'Assalamualaikum keluarga #nama_ortu, #enter #enter Terima kasih telah berkenan untuk mendaftar di PP Miftahul Ulum Bakid. #enter Kami mengingatkan bahwa hari ini Pendaftaran Santri Baru telah dibuka. #enter #enter Jangan lupa melanjutkan pengisian data santri di website bakid.id #enter #enter Terima kasih, #enter #panitia_psb_2023',
             ]
         );
     }
