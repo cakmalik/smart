7f1358f438377b1a15d3ab90752b0adaf7555488
cakmalik <hasinilmalik@gmail.com>
2023-07-29 04:35:25 +0700

    message edit and test

--- resources/views/bakid/setting/format_message/index.blade.php
+++ resources/views/bakid/setting/format_message/index.blade.php
@@ -14,25 +14,27 @@
                         <p class="text-center">No setting found.</p>
                     </x-slot>
                     @cell('action', $format_message)
-                        {{-- <div class="flex gap-1 ">
-                            <Link href="{{ route('user.show', $users->id) }}"
-                                class="rounded-full p-1 bg-slate-500 text-white hover:bg-green-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="w-5 h-5 p-1">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM10.5 7.5v6m3-3h-6" />
+                        <div class="flex gap-1 ">
+                            <Link modal href="{{ route('format-message.edit', $format_message->id) }}"
+                                class="rounded-full p-1 bg-slate-500 text-white hover:bg-green-500 me-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                viewBox="0 0 256 256">
+                                <path
+                                    d="M227.31,73.37,182.63,28.68a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H92.69A15.86,15.86,0,0,0,104,219.31L227.31,96a16,16,0,0,0,0-22.63ZM51.31,160,136,75.31,152.69,92,68,176.68ZM48,179.31,76.69,208H48Zm48,25.38L79.31,188,164,103.31,180.69,120Zm96-96L147.31,64l24-24L216,84.68Z">
+                                </path>
                             </svg>
+                            </Link>
+                            <Link href="{{ route('tes.message') }}"
+                                class="rounded-full p-1 bg-slate-500 text-white hover:bg-green-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                viewBox="0 0 256 256">
+                                <path
+                                    d="M223.87,114l-168-95.89A16,16,0,0,0,32.93,37.32l31,90.47a.42.42,0,0,0,0,.1.3.3,0,0,0,0,.1l-31,90.67A16,16,0,0,0,48,240a16.14,16.14,0,0,0,7.92-2.1l167.91-96.05a16,16,0,0,0,.05-27.89ZM48,224l0-.09L78.14,136H136a8,8,0,0,0,0-16H78.22L48.06,32.12,48,32l168,95.83Z">
+                                </path>
+                            </svg>
+                            </Link>
 
-                            </Link>
-                            <Link modal href="{{ route('user.edit', $users->id) }}"
-                                class="rounded-full p-1 bg-slate-500 text-white hover:bg-green-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="w-5 h-5 p-1">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                            </svg>
-                            </Link>
-                        </div> --}}
+                        </div>
                     @endcell
                 </x-splade-table>
 
