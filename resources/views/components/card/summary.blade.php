7191c255486100b714e40b8f84187eb120f355ce
cakmalik <hasinilmalik@gmail.com>
2023-07-30 14:00:49 +0700

    setup role and permission

--- resources/views/components/card/summary.blade.php
+++ resources/views/components/card/summary.blade.php
@@ -1,4 +1,4 @@
 <div
-    class="bg-white/50 hover:bg-white py-3 px-5 rounded-lg shadow-lg text-white text-center backdrop-blur-md border-[1px] border-white">
-    sdf
+    class="bg-white/50 hover:bg-white py-3 px-5 rounded-lg shadow-lg text-center backdrop-blur-md border-[1px] border-white">
+    250
 </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,29 +6,43 @@
             <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 {{ __('Dashboard') }}
             </h2>
-            <Link confirm="Apakah yakin keluar?" confirm-button="Ya!" cancel-button="Tidak" href="{{ route('logout') }}"
-                method="post">
-            Keluar
-            </Link>
+            @hasrole('santri')
+                <Link confirm="Apakah yakin keluar?" confirm-button="Ya!" cancel-button="Tidak" href="{{ route('logout') }}"
+                    method="post">
+                Keluar
+                </Link>
+            @endhasrole
         </div>
         </x-slot>
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 ">
-                @if (auth()->user()->students->count() != 0)
+                @if (roleName() == 'santri' &&
+                        auth()->user()->students->count() != 0)
                     <div class="grid grid-cols-2 gap-2 mb-4 sm:grid-cols-4 sm:gap-3">
-                        {{-- <x-card.summary />
                         <x-card.summary />
                         <x-card.summary />
-                        <x-card.summary /> --}}
+                        <x-card.summary />
+                        <x-card.summary />
                     </div>
                     <x-splade-rehydrate on="done-admission">
                         <x-dashboard.santri-baru :data="$x" />
                     </x-splade-rehydrate>
                 @else
-                    <div class="p-5 overflow-hidden shadow-xl bg-white/30 sm:rounded-lg backdrop-blur-md">
-                        <x-welcome />
-                    </div>
+                    {{-- <div class="p-5 overflow-hidden shadow-xl bg-white/30 sm:rounded-lg backdrop-blur-md"> --}}
+
+                    {{-- <x-welcome /> --}}
+                    @switch(roleName())
+                        @case('admin')
+                        @break
+
+                        @case('sekretaris')
+                            @include('dashboard.sekretaris')
+                        @break
+
+                        @default
+                    @endswitch
+                    {{-- </div> --}}
                 @endif
             </div>
         </div>
