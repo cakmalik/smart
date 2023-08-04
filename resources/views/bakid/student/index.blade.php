e93c10d81ed9811da5e85a9d6e162727f9c6cc70
cakmalik <hasinilmalik@gmail.com>
2023-08-05 01:50:21 +0700

    relayout

--- resources/views/bakid/student/index.blade.php
+++ resources/views/bakid/student/index.blade.php
@@ -25,18 +25,16 @@
                             </div>
 
                             <div class="relative flex gap-2 items-center mt-4 md:mt-0">
-                                <span class="absolute">
+                                {{-- <span class="absolute">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                         stroke-width="1.5" stroke="currentColor"
                                         class="w-5 h-5 mx-3 text-gray-400 dark:text-gray-600">
                                         <path stroke-linecap="round" stroke-linejoin="round"
                                             d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                                     </svg>
-                                </span>
-                                <div class="w-30">
-                                    <x-splade-select v-model="form.dormitory_id" :options="$dormitories" option-label="name"
-                                        option-value="id" placeholder="Filter Daerah" class="w-30" />
-                                </div>
+                                </span> --}}
+                                <x-splade-select v-model="form.dormitory_id" :options="$dormitories" option-label="name"
+                                    option-value="id" placeholder="Daerah" class="w-32" />
                                 <div class="flex gap-2">
                                     <input type="text" placeholder="Search"
                                         class="block w-full py-1.5 pr-1 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-52 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 "
@@ -169,8 +167,7 @@
                                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none"
                                                                     viewBox="0 0 24 24" stroke-width="1.5"
                                                                     stroke="currentColor" class="w-5 h-5 p-1">
-                                                                    <path stroke-linecap="round"
-                                                                        stroke-linejoin="round"
+                                                                    <path stroke-linecap="round" stroke-linejoin="round"
                                                                         d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM10.5 7.5v6m3-3h-6" />
                                                                 </svg>
 
