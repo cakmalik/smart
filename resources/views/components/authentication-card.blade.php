274e1cdc98be5ae4b21535008fd2555ae7b9b910
cakmalik <hasinilmalik@gmail.com>
2023-08-05 01:50:48 +0700

    relayout

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,11 +1,11 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100"
+<div class="min-h-screen flex flex-col justify-center items-center pt-6 sm:pt-0 bg-gray-100"
     style="background-image: url('{{ asset('bakid/bg-blur.jpg') }}'); background-size: cover; background-position: center;background-attachment: fixed;">
     <div>
         {{ $logo }}
     </div>
 
     <div
-        class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-xl  bg-white/40 backdrop-blur-md ">
+        class="w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-xl  bg-white/40 backdrop-blur-md ">
         {{ $slot }}
     </div>
 </div>
