6ef6967c546f86b100f4ce34d72b2d7982483505
cakmalik <hasinilmalik@gmail.com>
2023-08-05 01:48:35 +0700

    educational background relationship

--- app/Models/Student/StudentEducationalBackground.php
+++ app/Models/Student/StudentEducationalBackground.php
@@ -2,10 +2,17 @@
 
 namespace App\Models\Student;
 
+use App\Models\Student;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class StudentEducationalBackground extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
 }
