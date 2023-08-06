83de9583e0231d990c60578baa3c0d21f4a8db23
cakmalik <hasinilmalik@gmail.com>
2023-08-06 22:51:58 +0700

    change bg setting

--- app/Http/Controllers/BakidSettingController.php
+++ app/Http/Controllers/BakidSettingController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\BakidSetting;
+use Illuminate\Http\Request;
+use Psy\Formatter\Formatter;
 use ProtoneMedia\Splade\Facades\Toast;
 use App\Http\Requests\StoreBakidSettingRequest;
 use App\Http\Requests\UpdateBakidSettingRequest;
-use Psy\Formatter\Formatter;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class BakidSettingController extends Controller
 {
@@ -66,5 +69,74 @@ class BakidSettingController extends Controller
                 ->autoDismiss(5);
         }
         return back();
+    }
+
+    public function changeAccessCode(Request $request)
+    {
+        $request->validate([
+            'old_password' => 'required|min:1',
+            'new_password' => 'required|min:1',
+            'confirm_password' => 'required|min:1|same:new_password'
+        ]);
+
+        if ($request->old_password != env('KODE_AKSES')) {
+            return back()->with('error', 'Password lama tidak sesuai');
+        }
+
+        $password = env('KODE_AKSES');
+        $newPassword = $request->new_password;
+
+        $path = base_path('.env');
+        $getContent = file_get_contents($path);
+
+        if (file_exists($path)) {
+            file_put_contents($path, str_replace('KODE_AKSES=' . $password, 'KODE_AKSES=' . $newPassword, $getContent));
+        }
+        // return
+        return back()->with('success', 'Kode akses diubah');
+    }
+
+
+    function changeBackground()
+    {
+        $current_bg = env('CURRENT_BACKGROUND');
+        $path = base_path('.env');
+        $getContent = file_get_contents($path);
+
+        // Mendapatkan daftar nama file dari folder "public/bg"
+        $backgroundFiles = $this->getBackgroundFiles();
+
+        // Menghitung indeks berikutnya berdasarkan current_background
+        $currentIndex = array_search($current_bg, $backgroundFiles);
+        $nextIndex = ($currentIndex === false) ? 0 : ($currentIndex + 1) % count($backgroundFiles);
+
+        // Mendapatkan nama background baru berdasarkan indeks berikutnya
+        $newBg = $backgroundFiles[$nextIndex];
+
+        if (file_exists($path)) {
+            file_put_contents($path, str_replace('CURRENT_BACKGROUND=' . $current_bg, 'CURRENT_BACKGROUND=' . $newBg, $getContent));
+        }
+
+        Toast::message('Background diganti');
+        return back();
+    }
+
+
+
+    function getBackgroundFiles()
+    {
+        $path = public_path('bg');
+
+        if (File::exists($path)) {
+            $files = File::files($path);
+            $fileNames = [];
+
+            foreach ($files as $file) {
+                $fileNames[] = pathinfo($file, PATHINFO_FILENAME);
+            }
+            return $fileNames;
+        }
+
+        return [];
     }
 }
