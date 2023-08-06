0b496a310083de1cb0e79afa48e10dfedc2599ec
cakmalik <hasinilmalik@gmail.com>
2023-08-07 00:24:54 +0700

    add bg and make table blurry

--- resources/views/bakid/student/new_student/index.blade.php
+++ resources/views/bakid/student/new_student/index.blade.php
@@ -53,36 +53,36 @@
                             <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                                 <div class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
                                     <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                                        <thead class="bg-gray-50 dark:bg-gray-800">
+                                        <thead class="bg-gray-50/80 dark:bg-gray-800">
                                             <tr>
                                                 <th scope="col"
-                                                    class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                    class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-slate-700 dark:text-gray-400">
                                                     <button class="flex items-center gap-x-3 focus:outline-none">
                                                         <span>Nama</span>
                                                     </button>
                                                 </th>
 
                                                 <th scope="col"
-                                                    class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                    class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-slate-700 dark:text-gray-400">
                                                     Jenis kelamin
                                                 </th>
 
                                                 <th scope="col"
-                                                    class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                    class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-slate-700 dark:text-gray-400">
                                                     Daerah
                                                 </th>
 
                                                 <th scope="col"
-                                                    class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                    class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-slate-700 dark:text-gray-400">
                                                     Alamat
                                                 </th>
 
                                                 <th scope="col"
-                                                    class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                    class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-slate-700 dark:text-gray-400">
                                                     Keluarga</th>
 
                                                 <th scope="col"
-                                                    class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                                    class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-slate-700 dark:text-gray-400">
                                                     Phone</th>
 
                                                 <th scope="col" class="relative py-3.5 px-4">
@@ -91,7 +91,7 @@
                                             </tr>
                                         </thead>
                                         <tbody
-                                            class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                                            class="bg-white/40 backdrop-blur-md divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
                                             @forelse ($students as $i)
                                                 <tr>
                                                     <td
@@ -133,6 +133,7 @@
                                                     </td>
                                                     <td class="px-4 py-4 text-sm whitespace-nowrap">
                                                         <div class="flex items-center">
+                                                            ({{ $i->brothers_count }})
                                                             {{ $i->ayah }}
                                                         </div>
                                                     </td>
