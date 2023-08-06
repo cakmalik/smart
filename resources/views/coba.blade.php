90f8736c27a1b2d01800cab11693c5a7d6120345
cakmalik <hasinilmalik@gmail.com>
2023-08-06 22:57:35 +0700

    ktp size

---
+++ resources/views/coba.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>KTP Lookalike</title>
+    <style>
+        /* Set ukuran container */
+        body {
+            width: 85.6mm;
+            height: 53.98mm;
+            margin: 0;
+            padding: 0;
+        }
+
+        /* Set ukuran gambar KTP */
+        img {
+            width: 85.6mm;
+            height: 53.98mm;
+        }
+
+        /* Styling tambahan, sesuaikan dengan kebutuhan Anda */
+        .personal-info {
+            font-size: 12px;
+            text-align: center;
+            padding-top: 45mm;
+        }
+    </style>
+</head>
+
+<body>
+    <div>
+        <!-- Ganti URL_GAMBAR_KTP dengan URL gambar KTP yang ingin digunakan -->
+        <img src="URL_GAMBAR_KTP" alt="KTP">
+        <div class="personal-info">
+            <!-- Isi dengan data personal yang sesuai -->
+            <p>Nama: John Doe</p>
+            <p>Nomor KTP: 1234567890</p>
+            <p>Tanggal Lahir: 01 Januari 1990</p>
+            <p>Alamat: Jalan Contoh No. 123</p>
+        </div>
+    </div>
+</body>
+
+</html>
