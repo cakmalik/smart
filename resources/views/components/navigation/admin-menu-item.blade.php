8795fe0fc96e0142659d5da5da9931e10df9dcfe
cakmalik <hasinilmalik@gmail.com>
2023-08-10 04:43:58 +0700

    layout bottom menu

---
+++ resources/views/components/navigation/admin-menu-item.blade.php
@@ -0,0 +1,10 @@
+ <div class="w-full h-12">
+     <div class="flex flex-col gap-1 items-center justify-between ">
+         <div class="w-2/3">
+             <div class="rounded-full bg-gray-200 flex justify-center items-center py-1"><i
+                     class="ph-fill ph-user-list"></i>
+             </div>
+         </div>
+         <span class="font-bold">Nama</span>
+     </div>
+ </div>
