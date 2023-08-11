653782877959acb35d64b38e0a0df1f9cd1c28ee
cakmalik <hasinilmalik@gmail.com>
2023-08-12 02:54:48 +0700

    refactor

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -8,8 +8,7 @@
     @isset($header)
         <header class="text-center sm:text-start shadow bg-wa-teal2  backdrop-filter backdrop-blur-lg">
             <div class="max-w-7xl mx-auto py-3  px-4 sm:px-6 lg:px-8 text-white">
-                <div class="flex items-center justify-start gap-3 ">
-                    <img src="{{ asset('bakid/logo-ppmu.png') }}" alt="Logo" class="w-10 h-10 sm:hidden" />
+                <div class="flex items-center justify-between gap-3 ">
                     {{ $header }}
                 </div>
             </div>
