6c4b88eb40b5ca871ff11d3597f0e86c78dea82d
cakmalik <hasinilmalik@gmail.com>
2023-08-14 04:16:35 +0700

    invoice on wali update

--- resources/views/invoice/list.blade.php
+++ resources/views/invoice/list.blade.php
@@ -1,70 +1,76 @@
 <x-splade-modal max-width="xl">
-    <x-splade-toggle data="isHistory">
+    <x-splade-toggle data="isCompany">
         <div class="flex justify-between mt-6 gap-1">
             <button class="w-full py-2 border border-slate-400 rounded-l-md"
-                :class="{ 'bg-green-500 text-white': !isHistory }" @click.prevent="setToggle('isHistory', false)">List
+                :class="{ 'bg-green-500 text-white': !isCompany }" @click.prevent="toggle('isCompany')">List
                 tagihan</button>
             <button class="w-full py-2 border border-slate-400 rounded-r-md"
-                :class="{ 'bg-green-500 text-white': isHistory }"
-                @click.prevent="setToggle('isHistory', true)">Riwayat</button>
+                :class="{ 'bg-green-500 text-white': isCompany }" @click.prevent="toggle('isCompany')">Riwayat</button>
         </div>
-        <x-table v-if="isHistory==true">
-            <x-slot name="header">
-                <tr>
-                    <th scope="col" class="px-6 py-3">
-                        Judul
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Nominal
-                    </th>
-                    <th scope="col" class="px-6 py-3">
+        <div v-show="isCompany">
+            <x-table v-show="isCompany">
+                <x-slot name="header">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Judul
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Nominal
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Rincian
+                        </th>
+                    </tr>
+                </x-slot>
+                <tr
+                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td class="px-6 py-4">
+                        White
+                    </td>
+                    <td class="px-6 py-4">
+                        White
+                    </td>
+                    <td class="flex items-center px-6 py-4 space-x-3">
                         Rincian
-                    </th>
+                    </td>
                 </tr>
-            </x-slot>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="px-6 py-4">
-                    White
-                </td>
-                <td class="px-6 py-4">
-                    White
-                </td>
-                <td class="flex items-center px-6 py-4 space-x-3">
-                    Rincian
-                </td>
-            </tr>
-        </x-table>
-        <x-table v-if="!isHistory">
-            <x-slot name="header">
-                <tr>
-                    <th scope="col" class="px-6 py-3">
-                        Judul
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Nominal
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Status
-                    </th>
-                    <th scope="col" class="px-6 py-3">
+            </x-table>
+        </div>
+
+        <div class="" v-show="!isCompany">
+            <x-table>
+                <x-slot name="header">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Judul
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Nominal
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Status
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Rincian
+                        </th>
+                    </tr>
+                </x-slot>
+                <tr
+                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td class="px-6 py-4">
+                        White
+                    </td>
+                    <td class="px-6 py-4">
+                        White
+                    </td>
+                    <td class="px-6 py-4">
                         Rincian
-                    </th>
+                    </td>
+                    <td class="flex items-center px-6 py-4 space-x-3">
+                        Rincian
+                    </td>
                 </tr>
-            </x-slot>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="px-6 py-4">
-                    White
-                </td>
-                <td class="px-6 py-4">
-                    White
-                </td>
-                <td class="px-6 py-4">
-                    Rincian
-                </td>
-                <td class="flex items-center px-6 py-4 space-x-3">
-                    Rincian
-                </td>
-            </tr>
-        </x-table>
+            </x-table>
+        </div>
     </x-splade-toggle>
 </x-splade-modal>
