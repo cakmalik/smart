93a8451a8dc5fac0ad4b4184652582b6984bdab9
cakmalik <hasinilmalik@gmail.com>
2023-08-13 11:48:32 +0700

    fix and ok layout menu

--- resources/views/bakid/student/create.blade.php
+++ resources/views/bakid/student/create.blade.php
@@ -2,7 +2,7 @@
 
 <x-app-layout>
     {{-- <x-slot:header>
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-white leading-tight">
             {{ __('Santri Baru') }}
         </h2>
         </x-slot> --}}
--- resources/views/components/menu/admin-mobile-menu.blade.php
+++ resources/views/components/menu/admin-mobile-menu.blade.php
@@ -1,10 +1,11 @@
 <x-navigation.admin-menu-component name="{{ __('Dashboard') }}" icon="ph-house-line" :link="route('dashboard')"
     :active="request()->routeIs('dashboard')" />
-<x-navigation.admin-menu-component name="{{ __('Announcement') }}" icon="ph-house-line" :link="route('announcement.index')"
+<x-navigation.admin-menu-component name="{{ __('Announcement') }}" icon="ph-megaphone" :link="route('announcement.index')"
     :active="request()->routeIs('announcement.index')" />
-<x-navigation.admin-menu-component name="{{ __('Asrama') }}" icon="ph-house-line" link="" :active="request()->routeIs('dormitory.index')"
+
+{{-- cek route --}}
+<x-navigation.admin-menu-component name="{{ __('PSB') }}" icon="ph-house-line" link="" :active="request()->routeIs(['student.new', 'invoice.index'])"
     :submenu="[
-        ['name' => 'Asrama', 'link' => route('dormitory.index')],
-        ['name' => 'Formal', 'link' => route('formal.index')],
-        ['name' => 'Non-Formal', 'link' => route('informal.index')],
+        ['name' => 'Santri Baru', 'link' => route('student.new')],
+        ['name' => 'Rekap pembayaran', 'link' => route('invoice.index')],
     ]" />
