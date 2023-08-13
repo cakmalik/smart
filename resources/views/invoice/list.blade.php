b72a5fc4809340987baf60f12ef26af9bc3f6d5c
cakmalik <hasinilmalik@gmail.com>
2023-08-14 05:55:59 +0700

    get data from db and add empty state w lottie

--- resources/views/invoice/list.blade.php
+++ resources/views/invoice/list.blade.php
@@ -1,14 +1,14 @@
 <x-splade-modal max-width="xl">
-    <x-splade-toggle data="isCompany">
+    <x-splade-toggle data="isHistory">
         <div class="flex justify-between mt-6 gap-1">
             <button class="w-full py-2 border border-slate-400 rounded-l-md"
-                :class="{ 'bg-green-500 text-white': !isCompany }" @click.prevent="toggle('isCompany')">List
+                :class="{ 'bg-green-500 text-white': !isHistory }" @click.prevent="toggle('isHistory')">List
                 tagihan</button>
             <button class="w-full py-2 border border-slate-400 rounded-r-md"
-                :class="{ 'bg-green-500 text-white': isCompany }" @click.prevent="toggle('isCompany')">Riwayat</button>
+                :class="{ 'bg-green-500 text-white': isHistory }" @click.prevent="toggle('isHistory')">Riwayat</button>
         </div>
-        <div v-show="isCompany">
-            <x-table v-show="isCompany">
+        <div v-show="isHistory">
+            <x-table v-show="isHistory">
                 <x-slot name="header">
                     <tr>
                         <th scope="col" class="px-6 py-3">
@@ -20,24 +20,41 @@
                         <th scope="col" class="px-6 py-3">
                             Rincian
                         </th>
+                        <th scope="col" class="px-6 py-3">
+                            Status
+                        </th>
                     </tr>
                 </x-slot>
-                <tr
-                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <td class="px-6 py-4">
-                        White
-                    </td>
-                    <td class="px-6 py-4">
-                        White
-                    </td>
-                    <td class="flex items-center px-6 py-4 space-x-3">
-                        Rincian
-                    </td>
-                </tr>
+                @forelse ($histories as $i)
+                    <tr
+                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="px-6 py-4">
+                            <Link class="flex items-center justify-center cursor-pointer"
+                                href="{{ route('invoice.show', $i->invoice_number) }}">
+                            <i class="ph-fill ph-magnifying-glass-plus"></i>
+                            <span>{{ $i->invoice_number }}</span>
+                            </Link>
+                        </td>
+
+                        <td class="px-6 py-4">
+                            {{ $i->description }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $i->amount }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $i->status }}
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="4"> Belum ada riwayat pembayaran</td>
+                    </tr>
+                @endforelse
             </x-table>
         </div>
 
-        <div class="" v-show="!isCompany">
+        <div class="" v-show="!isHistory">
             <x-table>
                 <x-slot name="header">
                     <tr>
@@ -55,21 +72,40 @@
                         </th>
                     </tr>
                 </x-slot>
-                <tr
-                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <td class="px-6 py-4">
-                        White
-                    </td>
-                    <td class="px-6 py-4">
-                        White
-                    </td>
-                    <td class="px-6 py-4">
-                        Rincian
-                    </td>
-                    <td class="flex items-center px-6 py-4 space-x-3">
-                        Rincian
-                    </td>
-                </tr>
+                @forelse ($invoices as $i)
+                    <tr
+                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="px-6 py-4">
+                            <Link class="flex items-center justify-center cursor-pointer"
+                                href="{{ route('invoice.show', $i->invoice_number) }}">
+                            <i class="ph-fill ph-magnifying-glass-plus"></i>
+                            <span>{{ $i->invoice_number }}</span>
+                            </Link>
+                        </td>
+
+                        <td class="px-6 py-4">
+                            {{ $i->description }}
+                        </td>
+                        <td class="flex items-center px-6 py-4 space-x-3">
+                            {{ $i->amount }}
+                        </td>
+                        <td class="flex items-center px-6 py-4 space-x-3">
+                            {{ $i->status }}
+                        </td>
+                    </tr>
+                @empty
+                    <tr class="text-center h-40">
+                        <td colspan="4">
+                            <div class="flex flex-col justify-center items-center p-4 gap-1">
+                                <lottie-player
+                                    src="https://lottie.host/e1929754-8ae8-40ae-af73-de3d132e5fb6/ZVkbeMfTvv.json"
+                                    background="transparent" speed="1" style="width: 300px; height: 300px;" loop
+                                    autoplay></lottie-player>
+                                <span class="text-lg">Belum tagihan terbaru</span>
+                            </div>
+                        </td>
+                    </tr>
+                @endforelse
             </x-table>
         </div>
     </x-splade-toggle>
--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://unpkg.com/@phosphor-icons/web@2.0.3"></script>
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+
     <style>
         .ph-fill {
             font-size: 27px;
