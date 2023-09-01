bade7ae64d98768a2f1725972a32d600d4bd1812
cakmalik <hasinilmalik@gmail.com>
2023-09-01 11:22:21 +0700

    add mutation

---
+++ app/Http/Controllers/MutationController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MutationController extends Controller
+{
+    public function index()
+    {
+        
+        return view('bakid.mutation.index');
+    }
+}
