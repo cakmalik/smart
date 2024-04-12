3a07fcd98ac6d069a4786e8fe539d730024ca8c0
Cak Malik <hasinilmalik@gmail.com>
2024-04-13 06:19:00 +0700

    update

--- resources/views/dashboard/sekretaris.blade.php
+++ resources/views/dashboard/sekretaris.blade.php
@@ -1,8 +1,16 @@
 <x-add.ayat />
 <div class="grid grid-cols-1 md:grid-cols-4 gap-2">
-    <x-card.summary title="Santri Baru" value="453" color="atlantis" />
-    <x-card.summary title="Total Santri " value="453" color="danube" />
-    <x-card.summary title="Menunggu persetujuan" value="453" color="chilean" />
-    <x-card.summary title="Mutasi" value="453" color="mantis" />
+    <Link href="{{ route('student.new') }}">
+        <x-card.summary title="Santri Baru" value="453" color="atlantis" />
+    </Link>
+    <Link href="{{ route('student.new') }}">
+            <x-card.summary title="Total Santri " value="453" color="danube" />
+    </Link>
+    <Link href="{{ route('student.new') }}">
+        <x-card.summary title="Menunggu persetujuan" value="453" color="chilean" />
+    </Link>
+    <Link href="{{ route('student.new') }}">
+        <x-card.summary title="Mutasi" value="453" color="mantis" />
+    </Link>
 </div>
 {{-- statistic --}}
