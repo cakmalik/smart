b64e528a46d53446a9cfd040f06f49591e32c782
Cak Malik <hasinilmalik@gmail.com>
2024-04-13 06:15:04 +0700

    update dashboard and color ui tailwind

--- resources/views/components/card/summary.blade.php
+++ resources/views/components/card/summary.blade.php
@@ -1,4 +1,40 @@
-<div
-    class="bg-white/50 hover:bg-white py-3 px-5 rounded-lg shadow-lg text-center backdrop-blur-md border-[1px] border-white">
-    250
+@props(['title' => '', 'value' => '', 'color' => 'atlantis'])
+
+@php
+    // Daftar warna yang valid untuk digunakan
+    $colors = [
+        'atlantis' => [
+            'text' => 'atlantis-800',
+            'bg' => 'bg-atlantis-200',
+        ],
+        'danube' => [
+            'text' => 'danube-800',
+            'bg' => 'bg-danube-200',
+        ],
+        'chilean' => [
+            'text' => 'chilean-800',
+            'bg' => 'bg-chilean-200',
+        ],
+        'mantis' => [
+            'text' => 'mantis-800',
+            'bg' => 'bg-mantis-200',
+        ],
+
+        // Tambahkan warna lain di sini jika diperlukan
+    ];
+
+    // Pastikan warna yang dipilih ada dalam daftar yang valid
+    $textColorClass = $colors[$color]['text'] ?? 'atlantis-800';
+    $bgColorClass = $colors[$color]['bg'] ?? 'bg-atlantis-200';
+@endphp
+
+<div {{ $attributes->class(['p-4 rounded-lg shadow', $bgColorClass]) }}>
+    <div class="flex flex-col sm:flex-row items-center justify-between text-center">
+        <div class="text-lg font-medium text-neutral-600">
+            {{ $title }}
+        </div>
+        <div class="text-lg font-medium text-{{ $textColorClass }}">
+            {{ $value }}
+        </div>
+    </div>
 </div>
--- resources/views/dashboard/sekretaris.blade.php
+++ resources/views/dashboard/sekretaris.blade.php
@@ -1,8 +1,8 @@
 <x-add.ayat />
-<div class="grid grid-cols-2 md:grid-cols-4 gap-2 p-3">
-    <x-card.summary />
-    <x-card.summary />
-    <x-card.summary />
-    <x-card.summary />
+<div class="grid grid-cols-1 md:grid-cols-4 gap-2">
+    <x-card.summary title="Santri Baru" value="453" color="atlantis" />
+    <x-card.summary title="Total Santri " value="453" color="danube" />
+    <x-card.summary title="Menunggu persetujuan" value="453" color="chilean" />
+    <x-card.summary title="Mutasi" value="453" color="mantis" />
 </div>
 {{-- statistic --}}
