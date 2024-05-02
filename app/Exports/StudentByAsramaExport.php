19477a93ca1876f30290ae677754130c17aab980
Cak Malik <hasinilmalik@gmail.com>
2024-05-03 02:10:11 +0700

    export job

--- app/Exports/StudentByAsramaExport.php
+++ app/Exports/StudentByAsramaExport.php
@@ -12,28 +12,23 @@ class StudentByAsramaExport implements FromQuery
     use Exportable;
 
     public $year;
-    public $dormitory_id;
-    public $room_id;
     
-    public function __construct(int $year, int $dormitory_id, int $room_id)
+    public function __construct(int $year)
     {
-        $this->year = $year;
-        $this->dormitory_id = $dormitory_id;
-        $this->room_id = $room_id;
-        
+        $this->year = $year;        
     }
 
     public function query()
     {
-        $q = Student::query()
-        ->with('dormitory', 'room')
-        ->whereYear('verified_at', $this->year)
-        ->whereHas('dormitory', function($q) {
-            $q->where('id', $this->dormitory_id);
-        })
-        ->whereHas('room', function($q) {
-            $q->where('id', $this->room_id);
-        });
+        $q = Student::query();
+    //     ->with('dormitory', 'room')
+    //     ->whereYear('verified_at', $this->year)
+    //     ->whereHas('dormitory', function($q) {
+    //         $q->where('id', $this->dormitory_id);
+    //     })
+    //     ->whereHas('room', function($q) {
+    //         $q->where('id', $this->room_id);
+    //     });
 
         return $q; 
     }
--- app/Jobs/StudentAsramaExportJob.php
+++ app/Jobs/StudentAsramaExportJob.php
@@ -5,7 +5,10 @@ namespace App\Jobs;
 use Illuminate\Http\Request;
 use Illuminate\Bus\Queueable;
 use Illuminate\Support\Facades\Log;
+use App\Exports\StudentByAsramaExport;
+use App\Models\Utils\StudentExportHistory;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -22,7 +25,7 @@ class StudentAsramaExportJob implements ShouldQueue
     public int $year;
     public int $dormitory_id;
     public int $room_id;
-     
+
     public function __construct(string $category, int $year, int $dormitory_id, int $room_id)
     {
         $this->category = $category;
@@ -36,6 +39,40 @@ class StudentAsramaExportJob implements ShouldQueue
      */
     public function handle(): void
     {
-        Log::info('run: JobStudentAsramaExport');
+    try {
+        $exe = new StudentByAsramaExport($this->year);
+    
+        $filename = 'by-' . $this->category . '-' . $this->year . '(' . date('ymdHis') . ')';
+        $filePath = 'exports/' . $filename . '.xlsx'; // Path untuk menyimpan file di dalam direktori 'storage/app/exports'
+    
+        // Ekspor data siswa ke file Excel
+        $exe->download($filePath);
+    
+        // Pindahkan file Excel ke direktori 'storage/app/exports'
+        Storage::move($filePath, 'exports/' . $this->category . '.xlsx');
+    
+        // Simpan ke database
+        StudentExportHistory::create([
+            'file_name' => $filename,
+            'year' => $this->year,
+            'type' => $this->category,
+            'status' => 'success',
+        ]);
+    } catch (\Exception $e) {
+        // Tangani exception yang terjadi
+        // Contoh: Log pesan kesalahan atau tindakan pemulihan lainnya
+        Log::error('Error exporting and storing data: ' . $e->getMessage());
+    
+        // Ubah status ke 'failed' jika terjadi kesalahan
+        StudentExportHistory::create([
+            'file_name' => $filename ?? 'unknown',
+            'year' => $this->year ?? 'unknown',
+            'type' => $this->category ?? 'unknown',
+            'status' => 'failed',
+        ]);
+    
+        // Throw exception kembali untuk penanganan lebih lanjut jika perlu
+        throw $e;
     }
+    
 }
