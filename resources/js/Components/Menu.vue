a769836672d0f75676bfc3bc3e2e99603c8ee88a
Cak Malik <hasinilmalik@gmail.com>
2024-06-01 23:01:28 +0700

    update ui

--- resources/js/Components/Menu.vue
+++ resources/js/Components/Menu.vue
@@ -1,12 +1,14 @@
 <template>
     <div v-if="showMenu"
         class="min-h-screen w-full fixed top-0 left-0 bg-black/80 backdrop-blur-md z-[999999999999999999999999999999999999]">
-        <slot />
-        <div class="absolute bottom-3 right-3">
-            <button @click="showMenu = false"
-                class="rounded-full flex items-center justify-center p-2 text-center text-white group transition duration-300">
-                <i class="ph ph-x group-hover:scale-125"></i>
-            </button>
+        <div ref="target" class="w-full flex items-center justify-center max-w-sm bg-red- h-screen mx-auto">
+            <slot />
+            <div class="absolute bottom-3 right-3">
+                <button @click="showMenu = false"
+                    class="rounded-full flex items-center justify-center p-2 text-center text-white group transition duration-300">
+                    <i class="ph ph-x group-hover:scale-125"></i>
+                </button>
+            </div>
         </div>
     </div>
 </template>
@@ -16,7 +18,7 @@ import { inject } from "vue";
 
 const Splade = inject("$splade");
 
-import { useMouse, onKeyStroke, useMagicKeys, whenever } from "@vueuse/core";
+import { useMouse, onKeyStroke, useMagicKeys, whenever, onClickOutside } from "@vueuse/core";
 import { watch, watchEffect, ref } from "vue";
 
 const { ctrl, shift, space, a, escape } = useMagicKeys()
@@ -25,8 +27,9 @@ const showMenu = ref(false)
 whenever(ctrl, () => showMenu.value = !showMenu.value)
 whenever(escape, () => showMenu.value = false)
 
-function visitCheckout() {
-    console.log(123)
-    Splade.visit("/checkout");
-}
+const target = ref(null)
+
+onClickOutside(target, event =>
+    showMenu.value = false
+)
 </script>
--- resources/views/components/menu/new/main.blade.php
+++ resources/views/components/menu/new/main.blade.php
@@ -1,7 +1,165 @@
 <Menu>
-    <div class="w-full flex items-center justify-center max-w-xl h-screen mx-auto b-">
         <div class="grid grid-cols-3 gap-1 ">
-          <x-menu.new.menu-item />
+            {{-- <x-menu.new.menu-item /> --}}
+            <div class="flex flex-col gap-2 w-24 p-2 items-center rounded-lg text-center group cursor-pointer ">
+                <div
+                    class="w-12 h-12  rounded-full flex items-center justify-center bg-white group-hover:bg-green-500 transition duration-300 ease-in-out">
+                    <i
+                        class="ph-bold ph-house text-neutral-500 group-hover:text-white transition duration-300 ease-in-out"></i>
+                </div>
+                <span
+                    class="text-neutral-200 text-center text-sm group-hover:text-white  transition duration-300 ease-in-out">Home
+                    mede by</span>
+            </div>
+
+            <div class="">
+                <button id="dropdownHoverButton1" data-dropdown-toggle="dropdownHover1" data-dropdown-trigger="hover"
+                    type="button"
+                    class="flex flex-col gap-2 w-24 p-2 items-center rounded-lg text-center group cursor-pointer ">
+                    <div
+                        class="w-12 h-12  rounded-full flex items-center justify-center bg-white group-hover:bg-green-500 transition duration-300 ease-in-out">
+                        <i
+                            class="ph-bold ph-house text-neutral-500 group-hover:text-white transition duration-300 ease-in-out"></i>
+                    </div>
+                    <span
+                        class="text-neutral-200 text-center text-sm group-hover:text-white  transition duration-300 ease-in-out">Home</span>
+                </button>
+                <div id="dropdownHover1"
+                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton1">
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign
+                                out</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="">
+                <button id="dropdownHoverButton2" data-dropdown-toggle="dropdownHover2" data-dropdown-trigger="hover"
+                    type="button"
+                    class="flex flex-col gap-2 w-24 p-2 items-center rounded-lg text-center group cursor-pointer ">
+                    <div
+                        class="w-12 h-12  rounded-full flex items-center justify-center bg-white group-hover:bg-green-500 transition duration-300 ease-in-out">
+                        <i
+                            class="ph-bold ph-house text-neutral-500 group-hover:text-white transition duration-300 ease-in-out"></i>
+                    </div>
+                    <span
+                        class="text-neutral-200 text-center text-sm group-hover:text-white  transition duration-300 ease-in-out">Home
+                        mede by</span>
+                </button>
+                <div id="dropdownHover2"
+                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton2">
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign
+                                out</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="">
+                <button id="dropdownHoverButton3" data-dropdown-toggle="dropdownHover3" data-dropdown-trigger="hover"
+                    type="button"
+                    class="flex flex-col gap-2 w-24 p-2 items-center rounded-lg text-center group cursor-pointer ">
+                    <div
+                        class="w-12 h-12  rounded-full flex items-center justify-center bg-white group-hover:bg-green-500 transition duration-300 ease-in-out">
+                        <i
+                            class="ph-bold ph-house text-neutral-500 group-hover:text-white transition duration-300 ease-in-out"></i>
+                    </div>
+                    <span
+                        class="text-neutral-200 text-center text-sm group-hover:text-white  transition duration-300 ease-in-out">Home
+                        mede by</span>
+                </button>
+                <div id="dropdownHover3"
+                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton3">
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign
+                                out</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="">
+                <button id="dropdownHoverButton4" data-dropdown-toggle="dropdownHover4" data-dropdown-trigger="hover"
+                    type="button"
+                    class="flex flex-col gap-2 w-24 p-2 items-center rounded-lg text-center group cursor-pointer ">
+                    <div
+                        class="w-12 h-12  rounded-full flex items-center justify-center bg-white group-hover:bg-green-500 transition duration-300 ease-in-out">
+                        <i
+                            class="ph-bold ph-house text-neutral-500 group-hover:text-white transition duration-300 ease-in-out"></i>
+                    </div>
+                    <span
+                        class="text-neutral-200 text-center text-sm group-hover:text-white  transition duration-300 ease-in-out">Home
+                        mede by</span>
+                </button>
+                <div id="dropdownHover4"
+                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton4">
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign
+                                out</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
         </div>
-    </div>
+
+    <x-splade-script>
+        initFlowbite();
+    </x-splade-script>
 </Menu>
