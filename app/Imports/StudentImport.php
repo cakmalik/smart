45d1d344d9e001369545790a0381713f040f6a29
Cak Malik <hasinilmalik@gmail.com>
2024-06-04 21:37:32 +0700

    add column

--- app/Imports/StudentImport.php
+++ app/Imports/StudentImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\Student;
+use App\Models\StudentFamily;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\ToCollection;
@@ -17,19 +18,11 @@ class StudentImport implements ToCollection, WithHeadingRow
     {
         foreach ($rows as $row) {
 
-
-        // Store To users
-
-        // Store to students
-
-        // Store to student_families
-
-        // Store to room_students
-
-        // 
-
-
-
+            $this->_storeFamily($row);
+            $this->_storeUser($row);
+            $this->_storeStudent($row);
+            $this->_storeRoom($row);
+            
         }
     }
 
@@ -37,4 +30,70 @@ class StudentImport implements ToCollection, WithHeadingRow
     {
         return 1;
     }
+
+    private function _storeFamily($row){
+        try {
+
+            $sf = new StudentFamily();
+
+            $sf->father_name = $row['nama_ayah'];
+            $sf->father_nik = $row['no_nik'];
+            $sf->father_phone = $row['nama_ayah'];
+            $sf->father_education = $row['nama_ayah'];
+            $sf->father_job = $row['nama_ayah'];
+            $sf->father_income = $row['nama_ayah'];
+            $sf->father_status = $row['ayah_kandung'];
+            $sf->father_place_of_birth = $row['nama_ayah'];
+            $sf->father_date_of_birth = $row['nama_ayah'];
+
+            $sf->mother_name = $row['nama_ayah'];
+            $sf->mother_nik = $row['nama_ayah'];
+            $sf->mother_phone = $row['nama_ayah'];
+            $sf->mother_education = $row['nama_ayah'];
+            $sf->mother_job = $row['nama_ayah'];
+            $sf->mother_income = $row['nama_ayah'];
+            $sf->mother_status = $row['nama_ayah'];
+            $sf->mother_place_of_birth = $row['nama_ayah'];
+            $sf->mother_date_of_birth = $row['nama_ayah'];
+
+            $sf->guard_name = $row['nama_ayah'];
+            $sf->guard_nik = $row['nama_ayah'];
+            $sf->guard_phone = $row['nama_ayah'];
+            $sf->guard_education = $row['nama_ayah'];
+            $sf->guard_job = $row['nama_ayah'];
+            $sf->guard_income = $row['nama_ayah'];
+            $sf->parent_image = $row['nama_ayah'];
+
+            $sf->save();
+            
+        }catch(\Exception $e){
+            Log::error($e->getMessage(). ' at line ' . $e->getLine());
+            return false;
+        }
+    }
+
+    private function _storeUser($row){
+        try {
+        }catch(\Exception $e){
+            Log::error($e->getMessage(). ' at line ' . $e->getLine());
+            return false;
+        }
+    }
+    
+    private function _storeStudent($row){
+        try {
+        }catch(\Exception $e){
+            Log::error($e->getMessage(). ' at line ' . $e->getLine());
+            return false;
+        }
+    }
+
+    private function _storeRoom($row){
+        try {
+        }catch(\Exception $e){
+            Log::error($e->getMessage(). ' at line ' . $e->getLine());
+            return false;
+        }
+    }
+
 }
--- database/migrations/2024_06_04_205646_add_birth_student_family.php
+++ database/migrations/2024_06_04_205646_add_birth_student_family.php
@@ -12,6 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('student_families', function (Blueprint $table) {
+            $table->string('father_place_of_birth')->after('father_name')->nullable();
+            $table->date('father_date_of_birth')->after('father_name')->nullable();
+            $table->string('mother_place_of_birth')->after('mother_name')->nullable();
+            $table->date('mother_date_of_birth')->after('mother_name')->nullable();
             $table->string('father_status')->default('kandung')->after('father_name');
             $table->string('mother_status')->default('kandung')->after('mother_name');
         });
@@ -23,7 +27,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('student_families', function (Blueprint $table) {
-            $table->dropColumn(['father_status', 'mother_status']);
+            $table->dropColumn(['father_place_of_birth', 'father_date_of_birth', 'mother_place_of_birth', 'mother_date_of_birth', 'father_status', 'mother_status']);
         });
     }
 };
