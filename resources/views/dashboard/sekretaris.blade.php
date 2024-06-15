b7f2aa6a8111906d31a13bd8bb0d34e1eb46006a
Cak Malik <hasinilmalik@gmail.com>
2024-06-15 18:36:29 +0700

    update dashboard

--- resources/views/dashboard/sekretaris.blade.php
+++ resources/views/dashboard/sekretaris.blade.php
@@ -3,10 +3,10 @@
     <Link href="{{ route('student.new') }}">
     <x-card.summary title="Pendaftar" :value="$summary['pendaftar']" color="atlantis" />
     </Link>
-    <Link href="{{ route('student.new') }}">
+    <Link href="{{ route('student.index') }}">
     <x-card.summary title="Santri Laki" :value="$summary['student_l_count']" color="chilean" />
     </Link>
-    <Link href="{{ route('student.new') }}">
+    <Link href="{{ route('student.index') }}">
     <x-card.summary title="Santri Perempuan" :value="$summary['student_p_count']" color="danube" />
     </Link>
     <Link href="{{ route('invoice.index') }}">
@@ -15,7 +15,7 @@
 </div>
 
 <div class="grid grid-cols-1 md:grid-cols-3 gap-2 mt-3  mx-4 sm:mx-0">
-    <Link href="{{ route('invoice.index') }}" class="bg-slate-200 rounded-lg p-3">
+    <Link href="/student?filter[verified_at]={{ date('Y') }}" class="bg-slate-200 rounded-lg p-3">
     <div class="flex flex-col gap-2">
         <div class="flex justify-between items-center"><span>PSB {{ date('Y') }}</span> <span
                 class="bg-yellow-400 text-white text-xs p-1 rounded font-semibold">BELUM LUNAS</span></div>
@@ -24,7 +24,7 @@
     </div>
 
     </Link>
-    <Link href="{{ route('invoice.index') }}" class="bg-slate-200 rounded-lg p-3">
+    <Link href="/student?filter[verified_at]={{ date('Y') }}" class="bg-slate-200 rounded-lg p-3">
     <div class="flex flex-col gap-2">
         <div class="flex justify-between items-center"><span>PSB {{ date('Y') }}</span> <span
                 class="bg-green-400 text-white text-xs p-1 rounded font-semibold">LUNAS</span></div>
@@ -32,7 +32,7 @@
                 class="text-xl font-light"> / {{ $summary['psb_paid_count'] }}</span></div>
     </div>
     </Link>
-    <Link href="{{ route('invoice.index') }}" class="bg-slate-200 rounded-lg p-3">
+    <Link href="/student?filter[verified_at]={{ date('Y') }}" class="bg-slate-200 rounded-lg p-3">
     <div class="flex flex-col gap-2">
         <div class="flex justify-between items-center"><span>PSB {{ date('Y') }}</span> <span
                 class="bg-slate-800 text-white text-xs p-1 rounded font-semibold">SANTRI BARU</span></div>
