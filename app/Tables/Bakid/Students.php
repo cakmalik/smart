a86d7ea1ad066df24105c3cd18546af71e69c92c
Cak Malik <hasinilmalik@gmail.com>
2024-06-16 15:21:02 +0700

    update query

--- app/Tables/Bakid/Students.php
+++ app/Tables/Bakid/Students.php
@@ -42,20 +42,7 @@ class Students extends AbstractTable
      */
     public function for()
     {
-
-        $globalSearch = AllowedFilter::callback('global', function ($query, $value) {
-            $query->where(function ($query) use ($value) {
-                Collection::wrap($value)->each(function ($value) use ($query) {
-                    $query
-                        ->orWhere('name', 'LIKE', "%{$value}%");
-                });
-            });
-        });
- 
-        return QueryBuilder::for(Student::class)
-            ->defaultSort('name')
-            ->allowedSorts(['name'])
-            ->allowedFilters(['name','verified_at', $globalSearch]);
+        return Student::query()->with('user', 'room', 'dormitory')->whereNotNull('verified_at');
     }
 
     /**
@@ -68,25 +55,26 @@ class Students extends AbstractTable
     {
         $dormitories = Dormitory::filter(Auth::user()->gender == 'male' ? 'L' : 'P')->get();
 
-        $transformedArray = $dormitories->mapWithKeys(function ($dormitory) {
-            return [$dormitory['id'] => $dormitory['name']];
-        })->toArray();
+        $transformedArray = $dormitories
+            ->mapWithKeys(function ($dormitory) {
+                return [$dormitory['id'] => $dormitory['name']];
+            })
+            ->toArray();
 
         // Ambil tahun unik dari kolom 'verified_at' dalam model 'Student'
         // REMEMBER: MYSQL dan POSTGRES berbeda
         // kalau mysql seperti ini :
         // $years = DB::table('students')->whereNotNull('verified_at')->distinct()->pluck(DB::raw('YEAR(verified_at) as year'))->toArray();
 
-        $years = DB::table('students')
-        ->whereNotNull('verified_at')
-        ->distinct()
-        ->pluck(DB::raw('YEAR(verified_at) as year'))
-        ->toArray();
+        $years = DB::table('students')->whereNotNull('verified_at')->distinct()->pluck(DB::raw('YEAR(verified_at) as year'))->toArray();
 
         // Transformasikan array tahun ke format yang diinginkan menggunakan mapWithKeys
-        $transformedYears = collect($years)->sortDesc()->mapWithKeys(function ($year) {
-            return [$year => $year];
-        })->toArray();
+        $transformedYears = collect($years)
+            ->sortDesc()
+            ->mapWithKeys(function ($year) {
+                return [$year => $year];
+            })
+            ->toArray();
 
         // kalau postgres seperti ini :
         // $years = Student::selectRaw('distinct extract(year from verified_at) as year')->whereNotNull('verified_at')->pluck('year')->toArray();
@@ -101,26 +89,10 @@ class Students extends AbstractTable
             ->column('gender')
             ->column('asrama')
             ->column('family', 'saudara', canBeHidden: true)
-            ->column(
-                key: 'district',
-                label: 'District',
-                sortable: true,
-                canBeHidden: true
-            )
-            ->column(
-                key: 'city',
-                label: 'city',
-                sortable: true,
-                canBeHidden: true
-            )
+            ->column(key: 'district', label: 'District', sortable: true, canBeHidden: true)
+            ->column(key: 'city', label: 'city', sortable: true, canBeHidden: true)
 
-            ->selectFilter(
-                key: 'dormitory.id',
-                options: $transformedArray,
-                label: 'Asrama',
-                noFilterOption: true,
-                noFilterOptionLabel: 'Semua'
-            )
+            ->selectFilter(key: 'dormitory.id', options: $transformedArray, label: 'Asrama', noFilterOption: true, noFilterOptionLabel: 'Semua')
             // use filter search with callback
             ->selectFilter(
                 key: 'verified_at',
@@ -129,7 +101,7 @@ class Students extends AbstractTable
                 noFilterOption: true,
                 noFilterOptionLabel: 'Semua',
             )
-            ->rowModal(fn (Student $s) => route('student.show', ['student' => $s->id]))
+            ->rowModal(fn(Student $s) => route('student.show', ['student' => $s->id]))
             ->paginate(10);
     }
 }
