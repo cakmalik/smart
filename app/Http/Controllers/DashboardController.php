fc00b354aa310b11a8b303ad66a7e9951e6279b2
Cak Malik <hasinilmalik@gmail.com>
2024-06-18 16:50:38 +0700

    fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,11 +17,52 @@ use App\Models\Informal\InformalEducationClass;
 class DashboardController extends Controller
 {
     
+    private $invoiceService;
+
+    public function __construct(InvoiceService $invoiceService)
+    {
+        $this->invoiceService = $invoiceService;
+    }
 
     public function index()
     {
-        return view('dashboard');
+        $invoices_psb = $invoices = $this->invoiceService->getInvoicesByUserAndCode(auth()->user()->id, 'psb');
+        $x['formal'] = FormalEducation::get(['name', 'level', 'id']);
+        $newFormalEducation = new FormalEducation();
+        $newFormalEducation->name = 'Lainnya';
+        $newFormalEducation->level = '';
+        $newFormalEducation->id = 0;
+        $x['formal']->push($newFormalEducation);
+        // dd($x['formal']);
+        $x['informal'] = InformalEducation::all(['name', 'level', 'id']);
+        $newFormalEducation = new InformalEducation();
+        $newFormalEducation->name = 'Lainnya';
+        $newFormalEducation->level = '';
+        $newFormalEducation->id = 0;
+        $x['informal']->push($newFormalEducation);
+
+        $user = Auth::user();
+        if ($user->students->count() > 0) {
+            $x['students'] = Student::where('user_id', auth()->user()->id)->whereNull('education_updated')->orderByDesc('id')->get();
+            $x['studentsWithoutRooms'] = $this->getStudentsWithoutRooms($user);
+            $x['invoices_psb'] = $invoices_psb;
+        } else {
+            $x['students'] = [];
+        }
+
+        $count_new_students = Student::where('status', 'waiting')->count();
+        $count_students = Student::where('status', 'accepted')->count();
+        // dd($count_new_students, $count_students,);
+
+        $summary = [
+            'new_students' => $count_new_students,
+            'students' => $count_students,
+            'approval' => 0,
+            'mutation' => 0
+        ];
+        return view('dashboard', compact('x', 'summary'));
     }
+
 
     public function getStudentsWithoutRooms($user)
     {
