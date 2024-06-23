352a96f72891a12a7365d7651102de3b198d8fa5
Cak Malik <hasinilmalik@gmail.com>
2024-06-23 10:04:42 +0700

    update resource

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -135,10 +135,10 @@ class DocumentController extends Controller
                 $file_name = $dataSantri->nis . '.jpg';
                 $path = 'storage/kts/' . $file_name;
 
-                if (file_exists(public_path($path))) {
-                    // Jika file sudah ada, hapus file tersebut
-                    unlink(public_path($path));
-                }
+                // if (file_exists(public_path($path))) {
+                //     // Jika file sudah ada, hapus file tersebut
+                //     unlink(public_path($path));
+                // }
                 
                 $image->save(public_path($path));
                 return response()->download(public_path($path));
@@ -302,10 +302,10 @@ class DocumentController extends Controller
                 $file_name = $dataSantri->user?->kk . '.jpg';
                 $path = 'storage/k_mahram/' . $file_name;
 
-                if (file_exists(public_path($path))) {
-                    // Jika file sudah ada, hapus file tersebut
-                    unlink(public_path($path));
-                }
+                // if (file_exists(public_path($path))) {
+                //     // Jika file sudah ada, hapus file tersebut
+                //     unlink(public_path($path));
+                // }
                 
                 $image->save(public_path($path));
                 return response()->download(public_path($path));
